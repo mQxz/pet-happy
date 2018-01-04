50496cebd2c01f1a41cac97c5d6716aaa14db00c
mQxz <33171126+mQxz@users.noreply.github.com>
2018-01-04 23:41:43 +0800

    login01

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import 'styles/bases/border.css'
+import 'styles/bases/reset.css'
 
 Vue.config.productionTip = false
 
---
+++ src/pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-main">
+    111111
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login'
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import '../../assets/styles/common/varibles.styl'
+  .page-main
+    background: $bgColor
+</style>
