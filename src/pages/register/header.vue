494eb9c4906bbac387dc8b68e25139ea68fbb89f
whoAY <1967075996@qq.com>
2018-01-06 18:45:38 +0800

    完成基本布局

---
+++ src/pages/register/header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="close iconfont">&#xe600;</div>
+    </div>
+    <div class="reg-container">
+      <div class="reg-img-con">
+        <img src="../../assets/styles/img/logox2.png" alt="register" class="reg-img">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'reg-header'
+}
+</script>
+
+<style scoped lang="stylus">
+.header
+  height: .88rem
+  line-height: .88rem
+  .close
+    width: 1.44rem
+    line-height: .88rem
+    text-align: center
+    font-size: .4rem
+    color: #fff
+.reg-container
+  width: 100%
+  height: 3.4rem
+  .reg-img-con
+    overflow: hidden
+    width: 3.4rem
+    height: 0
+    margin: 0 auto
+    padding-bottom: 100%
+    .reg-img
+      width: 100%
+</style>
+
+
---
+++ src/pages/register/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="page-main">
+    <reg-header></reg-header>
+    <div class="input-container">
+      <div class="username-con input-con">
+        <input type="text" class="username input" placeholder="请输入你的账号">
+      </div>
+      <div class="password-con input-con">
+        <input type="text" class="password input" placeholder="请输入你的密码">
+      </div>
+      <div class="code-con input-con">
+        <input type="text" class="code input" placeholder="请输入验证码">
+        <button class="get-code">获取验证码</button>
+      </div>
+      <div class="register-submit">
+        <p class="title">注册账户表示您同意《pet乐用户协议》条款</p>
+        <input type="button" class="register-btn" value="注册">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RegHeader from './header'
+export default {
+  name: 'register',
+  components: {
+    RegHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '../../assets/styles/common/varibles.styl'
+  .page-main
+    position: absolute
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    background: $bgColor
+    .input-container
+      height: 4.1rem
+      margin-bottom: .8rem
+      .input-con
+        display: flex
+        align-items: center
+        margin: 0 auto
+        width: 5.2rem
+        height: .8rem
+        border: .03rem solid #fff
+        border-radius: .15rem
+        margin: .2rem auto
+        .input
+          flex: 1
+          margin: 0 .2rem
+          background: $bgColor
+          font-size: .3rem
+          color: #fff
+        .input::placeholder
+          color: #fff
+      .code-con
+        .get-code
+          height: .64rem
+          width: 1.46rem
+          line-height: .64rem
+          text-align: center
+          font-size: .24rem
+          color: $bgColor
+          background: #fff
+          border-radius: .1rem
+          margin-right: .1rem
+      .register-submit
+        width: 100%
+        display: flex
+        flex-direction: column
+        align-items: center
+        margin-top: .9rem
+        .title
+          line-height: .66rem
+          font-size: .2rem
+          color: #fff
+        .register-btn
+          width: 5.2rem
+          line-height: .8rem
+          font-size: .42rem
+          font-weight: 900
+          color: $bgColor
+          background: #fff
+          border-radius: .1rem
+        
+</style>
+
+
